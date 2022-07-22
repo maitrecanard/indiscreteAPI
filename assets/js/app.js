3f40b7b11af5f1a6a539e0ea7ce16ee75c318987
Mathieu SIaudeau <44211309+maitrecanard@users.noreply.github.com>
2022-07-22 23:06:06 +0200

    Test affichage ban

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ const app= {
     link: 'http://testapi.mathieusiaudeau.fr/api/ban',
 
     init: function(){
-        app.createBan();
+        app.loadBan();
     },
 
     loadBan: function(){
@@ -20,17 +20,20 @@ const app= {
             return response.json();
         })
         .then(function(data) {
-            console.log(data)
+            app.createBan(data);
         })
 
     },
 
-    createBan: function() {
-        app.loadBan();
-        //const indis = document.querySelector('.indiscrete');
-        //const a = document.createElement('a')
-       // const img = document.createElement('img')
-        //img.setAttribute('src', )
+    createBan: function(data) {
+        const indis = document.querySelector('.indiscrete');
+        const a = document.createElement('a')
+        const img = document.createElement('img')
+        img.setAttribute('src', data.img);
+        a.setAttribute('href', data.url);
+        a.setAttribute('alt', data.img)
+        indis.append(a);
+        a.append(img);
     }
 }
 
